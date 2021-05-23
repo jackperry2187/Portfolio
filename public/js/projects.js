1a20cd732fc8ee27dbd71b266438895e1e370c46
Jackson Perry <jackperry2187@gmail.com>
2021-05-23 04:36:34 -0400

    Projects Client Side

---
+++ public/js/projects.js
@@ -0,0 +1,51 @@
+(function($) {
+    let searchGroup1 = $('#searchGroup1');
+    let searchGroup2 = $('#searchGroup2');
+    let skills = $('td');
+
+    const sortProjects = (x) => {
+        let clickedButton = $(`button:contains("${x}")`);
+        if(clickedButton.hasClass('btn-primary')) {
+            //clicked button is already highlighted
+            //update clicked button
+            clickedButton.attr('class', 'btn btn-secondary border border-dark');
+            //unhighlight skill elements
+            $('.col').find(`td:contains("${x}")`).attr("class", "");
+        }
+        else {
+            //clicked button is not already highlighted
+            //update clicked button
+            clickedButton.attr('class', 'btn btn-primary border border-dark');
+            //highlight skill elements
+            $('.col').find(`td:contains("${x}")`).attr("class", "bg-primary");
+        }
+        
+    }
+
+    const loadSkills = () => {
+        let skillSet = [];
+        for(let x of skills) {
+            if($(x).html() != "") skillSet.push($(x).html());
+        }
+        if(skillSet == []) console.log("That's pretty sad");
+        
+        skillSet = [...new Set(skillSet)];
+
+        for(let x of skillSet) {
+            let button = $('<button></button>').attr({
+                type: "button",
+                class: "btn btn-secondary border border-dark",
+            }).html(x);
+            button.on('click', () => sortProjects(x));
+            if(searchGroup1.children().length < skillSet.length/2) button.appendTo(searchGroup1);
+            else button.appendTo(searchGroup2);
+        }
+    }
+
+    const startUp = () => {
+        loadSkills();
+    };
+
+    startUp();
+
+})(window.jQuery);
