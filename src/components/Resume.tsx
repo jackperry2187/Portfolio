e26fb99ae02275c4ce8a93c70a5d71092a60544e
jackperry2187 <jackperry2187@gmail.com>
2022-08-01 14:03:35 -0400

    Updated resume

--- src/components/Resume.tsx
+++ src/components/Resume.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export const Resume = () => {
     return (
         <main className="px-3 my-3 w-100">
-            <embed src="https://drive.google.com/file/d/1c8Ku9jP74W94-QJRnGM5XhacLaOWpdCA/preview?usp=sharing" width="60%" height="1080px"/> 
+            <embed src="https://drive.google.com/file/d/13so5uc__1at4YYK9ZxFyjm3v7YcPhG_Z/preview?usp=sharing" width="60%" height="1080px"/> 
         </main>
     )
 }
