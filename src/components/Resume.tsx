ca5c23a797f040ea4c61f5a9702524c880f978e1
Jackson Perry <jackperry2187@gmail.com>
2022-08-28 13:06:34 -0400

    Update resume

--- src/components/Resume.tsx
+++ src/components/Resume.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export const Resume = () => {
     return (
         <main className="px-3 my-3 w-100">
-            <embed src="https://drive.google.com/file/d/13so5uc__1at4YYK9ZxFyjm3v7YcPhG_Z/preview?usp=sharing" width="60%" height="1080px"/> 
+            <embed src="https://drive.google.com/file/d/1Jhn3mLGQ001wOFUR8Bcg_HfTiTqWyMo7/preview?usp=sharing" width="60%" height="1080px"/> 
         </main>
     )
 }
